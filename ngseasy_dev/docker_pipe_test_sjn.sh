580cc405568fe61f77b0f7ada36ce710faa1213f
Dr Stephen Newhouse <stephen.j.newhouse@gmail.com>
2014-09-19 12:58:21 +0100

    minor fix

--- ngseasy_dev/docker_pipe_test_sjn.sh
+++ ngseasy_dev/docker_pipe_test_sjn.sh
@@ -7,7 +7,11 @@
 ## create project directories
 ##--------------------------------------------------##
 
-create_ngseasy_projects () {
+initiate_project () {
+# NSGeasy Project Initiation
+# Makes Project and Sample Directories needed for NGSesy
+# Stephen Newhouse <stephen.j.newhouse@gmail.com>
+# Amos Folarin <afolarin@gmail.com>
 
 #usage printing func
 usage()
@@ -21,7 +25,7 @@ ARGUMENTS:
 -c      Config pipeline file
 -d      Base directory for (fastq_raw, reference_genomes_b37, gatk_resources, ngs_projects, ngseasy_scripts)
 EXAMPLE USAGE:
-create_ngseasy_projects -c config.file.tsv -d /media/D/docker_ngs/ngseasy/
+initiate_project -c config.file.tsv -d /media/D/docker_ngs/ngseasy/
 EOF
 }
 
@@ -53,10 +57,147 @@ done
 	  usage;
 	  exit 1;
   fi
+#check exists.
+  if [[ ! -e ${config_tsv} ]] 
+  then
+	  echo " ${config_tsv} does not exist "
+	  usage;
+	  exit 1;
+  fi
   
 # make dirs
-run_ngseasy_make_dirs.sh ${config_tsv} ${host_vol_dir} 
+# run_ngseasy_initiate_project.sh ${config_tsv} ${host_vol_dir} 
+## Print input 
+echo  " NGS config_file  : [${config_file}]"
+echo  " NGS host_vol_dir : [${host_vol_di}]"
 
+echo  " Checking [${config_file}] format"
+
+  numfeilds=`awk '{print NF}' ${config_file} | sort -gr | sed '1q'`
+  numsamples=`wc -l ${config_file} | awk '{print $1}'`
+echo  " Number of samples : [$numsamples]"
+    
+  if [ "$numfeilds" -ne "15" ]
+    then
+    echo  " WARNING! Number of fields not equal to 15 for one of your samples You appear to have missing data in [${config_file}]"
+    echo  " Check  [${config_file}] format"
+    echo  " The [config.file] should contain the following 15 columns for each sample:-"
+    echo  " 
+      POJECT_ID
+      SAMPLE_ID
+      FASTQ1
+      FASTQ2
+      PROJECT_DIR
+      DNA_PREP_LIBRARY_ID
+      NGS_PLATFORM
+      NGS_TYPE
+      BED_ANNO
+      PIPELINE
+      ALIGNER
+      VARCALLER
+      GTMODEGATK
+      CLEANUP
+      NCPU"
+    echo  " Data are [TAB] delimited and each line should end in a [NEW LINE]"
+      exit 1
+    else
+   echo  " All Good...Proceeding to read : [${config_file}]"
+  fi
+
+# begin reading config file line by line
+  
+while read -r f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 f12 f13 f14 f15
+do
+
+# set varibales  
+  POJECT_ID=$f1
+  SAMPLE_ID=$f2
+  FASTQ1=$f3
+  FASTQ2=$f4
+  PROJECT_DIR=$f5
+  DNA_PREP_LIBRARY_ID=$f6
+  NGS_PLATFORM=$f7
+  NGS_TYPE=$f8
+  BED_ANNO=$f9
+  PIPELINE=$f10
+  ALIGNER=$f11
+  VARCALLER=$f12
+  GTMODEGATK=$f13
+  CLEANUP=$f14
+  NCPU=$f15
+  
+echo  "#------------------------------------------------------------#"
+echo  " Checking Project/Sample Directory Structure"
+echo  "#------------------------------------------------------------#"
+     echo " Checking Project Directory "   
+  if [ ! -d $PROJECT_DIR/${POJECT_ID} ]
+    then
+    echo  " Project Directory Does not exist" 
+    echo  " Setting up correct Project Directory"
+    echo ""
+    echo  " Making Directory : [$PROJECT_DIR/${POJECT_ID}/]"
+    echo  " Making Directory : [$PROJECT_DIR/${POJECT_ID}/config_files]"
+    echo  " Making Directory : [$PROJECT_DIR/${POJECT_ID}/cohort_vcfs]"
+    
+        mkdir ${PROJECT_DIR}/${POJECT_ID}/
+        mkdir ${PROJECT_DIR}/${POJECT_ID}/config_files
+        mkdir ${PROJECT_DIR}/${POJECT_ID}/cohort_vcfs
+        
+  else
+    echo  " Project Directory Exists"
+  fi
+
+     echo " Checking Sample Directory " 
+  if [ ! -d $PROJECT_DIR/${POJECT_ID}/$SAMPLE_ID ]  
+    then
+    echo " Sample Directory Does not exist"
+    echo " Setting up correct Sample Directory"
+    echo ""
+        mkdir ${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}
+        mkdir ${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}/fastq
+        mkdir ${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}/tmp
+        mkdir ${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}/alignments
+        mkdir ${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}/vcf
+        mkdir ${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}/reports
+        mkdir ${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}/config_files
+        
+    echo  " Making Sample Directory : [${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}]"
+    echo  " Making Sample Directory : [${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}/fastq]"
+    echo  " Making Sample Directory : [${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}/tmp]"
+    echo  " Making Sample Directory : [${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}/alignments]"
+    echo  " Making Sample Directory : [${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}/vcf]"
+    echo  " Making Sample Directory : [${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}/reports]"
+    echo  " Making Sample Directory : [${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}/config_files]"
+  
+  else
+    echo  " Sample Directory Exists "
+  fi
+  
+echo ""
+echo  "#------------------------------------------------------------#"
+echo  " NGSeasy Project/Sample Settings "
+echo  "#------------------------------------------------------------#"
+echo ""
+DATE=`date +"%d%m%y"`
+echo  " Run Date : [$DATE]"
+echo  " Project_Id : [$POJECT_ID]" 
+echo  " Project Directory : [$PROJECT_DIR]"
+echo  " Platform : [$NGS_PLATFORM]"
+echo  " NGS Type : [$NGS_TYPE]"
+echo  " Clean up TRUE/FALE : [$CLEANUP]"
+echo  " Number of cpu : [$NCPU]"
+echo  " Output Directory : [${PROJECT_DIR}/${POJECT_ID}/${SAMPLE_ID}]"
+echo ""
+
+echo  "#------------------------------------------------------------#"
+echo  " Call NGS Pipeline "
+echo  "#------------------------------------------------------------#"
+echo ""
+ 
+echo  " Saving Project configuration settings to : [${PROJECT_DIR}/${POJECT_ID}/config_files/project.config.file.${DATE}]"
+echo  "$DATE $f1 $f2 $f3 $f4 $f5 $f6 $f7 $f8 $f9 $f10 $f11 $f12 $f13 $f14 $f15 ${SAMPLE_ID}.${NGS_TYPE}.${NGS_PLATFORM}.${ALIGNER} ${PROJECT_DIR}/${POJECT_ID}" >> ${PROJECT_DIR}/${POJECT_ID}/config_files/project.config.file.${DATE};
+  
+done < ${config_file}
 }
 
 ##--------------------------------------------------##
@@ -352,7 +493,7 @@ aln_bowtie2 () {
   usage()
   {
   cat << EOF
-  This script sets up the NGSeasy docker novoalign container:
+  This script sets up the NGSeasy docker Bowtie2 container:
   See NGSEasy containerized instructions.
 
   ARGUMENTS:
@@ -394,12 +535,122 @@ sudo docker run \
 -P \
 --name aln_bowtie2 \
 --volumes-from volumes_container \
--t compbio/ngseasy-novoalign:v1.0 /sbin/my_init -- /bin/bash /home/pipeman/ngseasy_scripts/run_ngseasy_bowtie2.sh /home/pipeman/ngs_projects/${config_tsv}
+-t compbio/ngseasy-bowtie2:v1.0 /sbin/my_init -- /bin/bash /home/pipeman/ngseasy_scripts/run_ngseasy_bowtie2.sh /home/pipeman/ngs_projects/${config_tsv}
 
 }
 
 
+##--------------------------------------------------##
+## NGSeasy: aln_bwa
+##--------------------------------------------------##
 
+aln_bwa () {
+
+#usage printing func
+  usage()
+  {
+  cat << EOF
+  This script sets up the NGSeasy docker BWA container:
+  See NGSEasy containerized instructions.
+
+  ARGUMENTS:
+  -h      Flag: Show this help message
+  -c      NGSeasy project and run configureation file
+  EXAMPLE USAGE:
+  aln_bowtie2 -c config.file.tsv
+EOF 
+}
+
+#get options for command line args
+  while  getopts "h:c:" opt
+  do
+
+      case ${opt} in
+	  h)
+	  usage #print help
+	  exit 0
+	  ;;
+	  
+	  c)
+	  config_tsv=${OPTARG}
+	  ;;
+
+      esac
+  done
+
+#check exists.
+  if [[ ! -e ${config_tsv} ]] 
+  then
+	  echo " ${config_tsv} does not exist "
+	  usage;
+	  exit 1;
+  fi
+
+#run compbio/ngseasy-fastq
+sudo docker run \
+-d \
+-P \
+--name aln_bwa \
+--volumes-from volumes_container \
+-t compbio/ngseasy-bwa:v1.0 /sbin/my_init -- /bin/bash /home/pipeman/ngseasy_scripts/run_ngseasy_bwa.sh /home/pipeman/ngs_projects/${config_tsv}
+
+}
+
+##--------------------------------------------------##
+## NGSeasy: aln_stampy
+##--------------------------------------------------##
+
+aln_stampy () {
+
+#usage printing func
+  usage()
+  {
+  cat << EOF
+  This script sets up the NGSeasy docker BWA container:
+  See NGSEasy containerized instructions.
+
+  ARGUMENTS:
+  -h      Flag: Show this help message
+  -c      NGSeasy project and run configureation file
+  EXAMPLE USAGE:
+  aln_bowtie2 -c config.file.tsv
+EOF 
+}
+
+#get options for command line args
+  while  getopts "h:c:" opt
+  do
+
+      case ${opt} in
+	  h)
+	  usage #print help
+	  exit 0
+	  ;;
+	  
+	  c)
+	  config_tsv=${OPTARG}
+	  ;;
+
+      esac
+  done
+
+#check exists.
+  if [[ ! -e ${config_tsv} ]] 
+  then
+	  echo " ${config_tsv} does not exist "
+	  usage;
+	  exit 1;
+  fi
+
+#run compbio/ngseasy-fastq
+sudo docker run \
+-d \
+-P \
+--name aln_stampy \
+--volumes-from volumes_container \
+-t compbio/ngseasy-stampy:v1.0 /sbin/my_init -- /bin/bash /home/pipeman/ngseasy_scripts/run_ngseasy_stampy.sh /home/pipeman/ngs_projects/${config_tsv}
+
+}
 
   
 #### NOTES ##########################################################################################################
@@ -421,10 +672,10 @@ aln_novo
 aln_stampy
 aln_bwa
 aln_bowtie2
-aln_gem
-aln_gsnap
-aln_mrsfast
-aln_mrfast
+#aln_gem
+#aln_gsnap
+#aln_mrsfast
+#aln_mrfast
 add_read_group
 mark_dupes
 gatk_clean
