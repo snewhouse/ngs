8b41f6187c81bea8eb9ee24a7efd8645493661af
Dr Stephen Newhouse <stephen.j.newhouse@gmail.com>
2014-10-16 16:37:49 +0100

    minor updates

--- bin/test_run.sh
+++ bin/test_run.sh
@@ -1,1 +1,16 @@
-/media/container-vol/ngs_projects
+
+
+
+#Good Run on Google Cloud
+
+ngseasy_initiate_project -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects
+
+ngseasy_volumes_container-d /media/container-vol/ngs_projects
+
+ngseasy_initiate_fastq -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects
+
+## To Test
+
+
+ngseasy -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects
+
