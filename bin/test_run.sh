89a1c0733f091012a52e34abb1860e5fc4512d8c
Dr Stephen Newhouse <stephen.j.newhouse@gmail.com>
2014-10-17 11:17:14 +0100

    test_run

--- bin/test_run.sh
+++ bin/test_run.sh
@@ -10,7 +10,7 @@ ngseasy_volumes_container-d /media/container-vol/ngs_projects
 ngseasy_initiate_fastq -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects
 
 # get 
-sudo docker run -i -t co
+sudo docker run --name annovar_db_get --volumes_from data_volumes -i -t compbio/ngseasy-annovar:v0.9 /bin/bash 
 
 
 ## To Test
