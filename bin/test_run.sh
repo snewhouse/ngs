a984ee869b51562f1d110ec29110d05ea0f4142d
Dr Stephen Newhouse <stephen.j.newhouse@gmail.com>
2014-10-17 15:11:01 +0100

    minor

--- bin/test_run.sh
+++ bin/test_run.sh
@@ -5,14 +5,28 @@
 
 ngseasy_initiate_project -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects
 
-ngseasy_volumes_container-d /media/container-vol/ngs_projects
+ngseasy_volumes_container -d /media/container-vol/ngs_projects
 
 ngseasy_initiate_fastq -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects
 
 # get annovar databases
-sudo docker run --name annovar_db_get --volumes-from data_volumes -i -t compbio/ngseasy-annovar:v0.9.1 /bin/bash /usr/local/pipeline/annovar/get_annovar_gene_databases.sh
+sudo docker run \
+	--name annovar_db_get \
+	--volumes-from data_volumes \
+	-i \
+	-t \
+	compbio/ngseasy-annovar:v0.9.2 \
+	/bin/bash \
+	/usr/local/pipeline/annovar/get_annovar_gene_databases.sh
 
-sudo docker run --volumes-from data_volumes -i -t compbio/ngseasy-annovar:v0.9.1 /bin/bash /usr/local/pipeline/annovar/get_annovar_databases.sh
+sudo docker run \
+	--rm true \
+	--volumes-from data_volumes \
+	-i \
+	-t \
+	compbio/ngseasy-annovar:v0.9.1 \
+	/bin/bash \
+	/usr/local/pipeline/annovar/get_annovar_databases.sh
 
 ## To Test
 
