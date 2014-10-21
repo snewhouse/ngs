96799568bc36f8ce46b6fe5d8ebbc674fcac1b71
Dr Stephen Newhouse <stephen.j.newhouse@gmail.com>
2014-10-21 14:38:06 +0100

    aligner and misc options

--- bin/test_run.sh
+++ bin/test_run.sh
@@ -40,9 +40,14 @@ ngseasy_trimmomatic -c /media/container-vol/ngs_projects/config_files/example.co
 ngseasy_alignment -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects 
 
 
-sudo docker run -P --volumes-from volumes_container -i -t compbio/ngseasy-bwa:v0.9 $(/usr/local/pipeline/bwa-0.7.10/bwa mem -t 8 /home/pipeman/reference_genomes_b37/human_g1k_v37.fasta \
+sudo docker run -P \
+--volumes-from volumes_container \
+-t compbio/ngseasy-bwa:v0.9 /bin/bash -c "/usr/local/pipeline/bwa-0.7.10/bwa mem -t 8 /home/pipeman/reference_genomes_b37/human_g1k_v37.fasta \
 /home/pipeman/ngs_projects/ngs_example/NA12878/fastq/NA12878.WEX_1.filtered.fq.gz \
-/home/pipeman/ngs_projects/ngs_example/NA12878/fastq/NA12878.WEX_2.filtered.fq.gz > /home/pipeman/ngs_projects/ngs_example/NA12878/alignments/NA12878.WEX.ILLUMINA.stampy.raw.bwa.sam)
+/home/pipeman/ngs_projects/ngs_example/NA12878/fastq/NA12878.WEX_2.filtered.fq.gz \
+> /home/pipeman/ngs_projects/ngs_example/NA12878/alignments/NA12878.WEX.ILLUMINA.stampy.raw.bwa.sam"
+
+
 
 
 /home/pipeman/ngs_projects/ngs_example/NA12878/alignments/NA12878.WEX.ILLUMINA.stampy.raw.bwa.sam
