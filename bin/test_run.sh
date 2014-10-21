1fbf1541e41ba042acbce4e7b97507c73c15f9e7
Dr Stephen Newhouse <stephen.j.newhouse@gmail.com>
2014-10-21 16:35:15 +0100

    big fix

--- bin/test_run.sh
+++ bin/test_run.sh
@@ -40,6 +40,18 @@ ngseasy_trimmomatic -c /media/container-vol/ngs_projects/config_files/example.co
 ngseasy_alignment -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects 
 
 
+# Add Read Groups
+ngseasy_addreadgroup -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects 
+
+#Mark Dupes
+ngseasy_markduplicates -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects 
+
+
+
+*****
+
+#Messing around
+
 sudo docker run -P \
 --volumes-from volumes_container \
 -t compbio/ngseasy-bwa:v0.9 /bin/bash -c "/usr/local/pipeline/bwa-0.7.10/bwa mem -t 8 /home/pipeman/reference_genomes_b37/human_g1k_v37.fasta \
