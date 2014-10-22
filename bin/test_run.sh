26cff8f02016e65c095358347088b1d105d35b49
Dr Stephen Newhouse <stephen.j.newhouse@gmail.com>
2014-10-22 14:47:53 +0100

    minor bin/ngseasy_base_recal

--- bin/test_run.sh
+++ bin/test_run.sh
@@ -33,12 +33,14 @@ sudo docker run \
 # this creates new sample confog file from project confif file and feeds as input to pipeline ngseasy_fastq
 ngseasy -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects
 
+# sep call to ngseasy_fastqc
+ngseasy_fastqc -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects 
+
 # sep call to ngseasy_trimmomatic
 ngseasy_trimmomatic -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects 
 
 # run aligment 
 ngseasy_alignment -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects 
-
 
 # Add Read Groups
 ngseasy_addreadgroup -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects 
@@ -53,22 +55,5 @@ ngseasy_indel_realn -c /media/container-vol/ngs_projects/config_files/example.co
 ngseasy_base_recal -c /media/container-vol/ngs_projects/config_files/example.config.tsv -d /media/container-vol/ngs_projects 
 
 
+
 *****
-
-#Messing around
-
-sudo docker run -P \
---volumes-from volumes_container \
--t compbio/ngseasy-bwa:v0.9 /bin/bash -c "/usr/local/pipeline/bwa-0.7.10/bwa mem -t 8 /home/pipeman/reference_genomes_b37/human_g1k_v37.fasta \
-/home/pipeman/ngs_projects/ngs_example/NA12878/fastq/NA12878.WEX_1.filtered.fq.gz \
-/home/pipeman/ngs_projects/ngs_example/NA12878/fastq/NA12878.WEX_2.filtered.fq.gz \
-> /home/pipeman/ngs_projects/ngs_example/NA12878/alignments/NA12878.WEX.ILLUMINA.stampy.raw.bwa.sam"
-
-
-
-
-/home/pipeman/ngs_projects/ngs_example/NA12878/alignments/NA12878.WEX.ILLUMINA.stampy.raw.bwa.sam
-/home/pipeman/ngs_projects/ngs_example/NA12878/fastq/NA12878.WEX_1.filtered.fq.gz
-
-
-sudo docker run --volumes-from volumes_container -i -t compbio/ngseasy-bwa:v0.9 /bin/bash
